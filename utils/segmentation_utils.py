e09fbfa7916902e5704835652794b15838343619
JeremyM2000 <jeremy.meme@hotmail.fr>
2024-04-02 14:19:38 +0200

    add displayed accuracy

--- utils/segmentation_utils.py
+++ utils/segmentation_utils.py
@@ -52,11 +52,15 @@ def get_prediction(img, threshold):
     masks = masks[:pred_t+1]
     pred_boxes = pred_boxes[:pred_t+1]
     pred_class = pred_class[:pred_t+1]
-    return masks, pred_boxes, pred_class
+
+    pred_score_ok = pred_score[:pred_t+1]
+    print(pred_score_ok)
+
+    return masks, pred_boxes, pred_class, pred_score_ok
 
 
-def instance_segmentation_api(img_path, threshold=0.5, rect_th=3, text_size=1, text_th=3):
-    masks, boxes, pred_cls = get_prediction(img_path, threshold)
+def instance_segmentation_api(img_path, threshold=0.5, rect_th=2, text_size=1, text_th=2):
+    masks, boxes, pred_cls, pred_score = get_prediction(img_path, threshold)
     img = cv2.cvtColor(np.array(img_path), cv2.COLOR_BGR2RGB)
     for i in range(len(masks)):
         rgb_mask = random_colour_masks(masks[i])
@@ -66,6 +70,6 @@ def instance_segmentation_api(img_path, threshold=0.5, rect_th=3, text_size=1, t
         # Convert to integer if necessary
         pt2 = (int(boxes[i][1][0]), int(boxes[i][1][1]))
         cv2.rectangle(img, pt1, pt2, color=(0, 255, 0), thickness=rect_th)
-        cv2.putText(img, pred_cls[i], pt1, cv2.FONT_HERSHEY_SIMPLEX,
+        cv2.putText(img, pred_cls[i] + " : " + str(int(pred_score[i]*100)) + "%", pt1, cv2.FONT_HERSHEY_SIMPLEX,
                     text_size, (0, 255, 0), thickness=text_th)
     return img
